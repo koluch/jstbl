38f0c46ec24fb580ab1976b2119bbd86a9809304
Nikolai Mavrenkov <koluch@koluch.ru>
2015-11-27 00:34:10 +0300

    Change arguments format

--- jstbl.js
+++ jstbl.js
@@ -45,10 +45,10 @@ process.stdin.on('end', () => {
     var data = JSON.parse(result);
 
     var args = process.argv.slice(2).map((arg) => {
-        if(!/^--.+=.+$/.test(arg)) throw new Error("Bad arg format: " + arg);
+        if(!/^.+=.+$/.test(arg)) throw new Error("Bad arg format: " + arg);
         var parts = arg.split("=");
         return {
-            name:parts[0].replace(/^--/, ""),
+            name:parts[0],
             value:parts[1]
         };
     });
